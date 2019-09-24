1c0a14a82ead2dbb657908608c235ba9b2eb0a82
450093854 <450093854@qq.com>
2019-09-24 18:55:20 +0800

    Update PipelineTestApplicationTests.java

--- src/test/java/com/sgst/PipelineTestApplicationTests.java
+++ src/test/java/com/sgst/PipelineTestApplicationTests.java
@@ -11,28 +11,28 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.util.Iterator;
 import java.util.Set;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class PipelineTestApplicationTests {
+// @RunWith(SpringRunner.class)
+// @SpringBootTest
+// public class PipelineTestApplicationTests {
 
-    @Autowired
-    private StringRedisTemplate redisTemplate;
+//     @Autowired
+//     private StringRedisTemplate redisTemplate;
 
-    @Autowired
-    private UserMapper userMapper;
+//     @Autowired
+//     private UserMapper userMapper;
 
-    @Test
-    public void mysqlTest() {
-        // 查询user表中总记录数
-        System.out.println("user表总记录数为："+userMapper.selectCount(null));
-    }
+//     @Test
+//     public void mysqlTest() {
+//         // 查询user表中总记录数
+//         System.out.println("user表总记录数为："+userMapper.selectCount(null));
+//     }
 
-    @Test
-    public void redisTest(){
-        // 查询redis第一个数据库中所有的key
-        Set<String> keys = redisTemplate.keys("*");
-        System.out.println("redis中key总记录数为"+keys.size());
-    }
+//     @Test
+//     public void redisTest(){
+//         // 查询redis第一个数据库中所有的key
+//         Set<String> keys = redisTemplate.keys("*");
+//         System.out.println("redis中key总记录数为"+keys.size());
+//     }
 
-}
+// }
 
